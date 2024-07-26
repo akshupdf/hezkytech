8a5dce9947643d6ad75e57e0a8bc670a250cda29
akshupdf <pednekar.akshay9@gmail.com>
2024-07-26 10:42:26 +0530

    responsive v4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from '../src/image.jpg';
 import './App.css';
 
 function App() {
+
+  const email = 'varun@hezkyteh.com';
+  const subject = 'I want to inquire about';
+  const body = 'Hello, I would like to...';
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +16,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>REQUEST A CALL</p>
         <div className='input-box'>
-        <input placeholder='ENTER YOUR PHONE NUMBER' type='text' className='login-input'></input>
-        <button className='btn' >SEND</button>
+        <input placeholder='ENTER YOUR EMAIL ID' type='text' className='login-input'></input>
+        <button className='btn'><a  href={`mailto:${email}?subject=${subject}&body=${body}`}  >SEND</a> </button>
         </div>
       
       </header>
